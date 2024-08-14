12423b1fbd44b148b1681d1635a5d2afd884349b
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-14 06:58:07 +0000

    Summary of let and var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,3 +5,10 @@ console.log(number);
 var age=20
 
 console.log(age);
+
+//beware using var because it has scope issues //
+//because it becomes global variable while declaring and its function scoped//
+//var can be hoisted means we use it before declaring and value will be undefined //
+
+//let on the other hand is block scoped means cannot acces outside {} //
+// let can also be hoisted  and moved on top  but it will show reference error//
