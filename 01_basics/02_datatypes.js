c05632385ac3fdb135f2add9ed1ab5a82e81046d
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-14 07:09:06 +0000

    Summary of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+//Datatypes//
+
+Number //=> 2 to power 53//
+String
+BigInt // for big number purpose usually in bigger projects like stock markets//
+Boolean //true/false
+null //standalone value if we print typeof it will show object 
+undefined //not assigning value//
+Symbol // for unique components //
+Object
