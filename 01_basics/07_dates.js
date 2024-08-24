16945c6a0f922df8a0f0e2b6516aac9fc3270f9d
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-24 16:28:34 +0000

    update

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,2 +1,40 @@
 let myDate= new Date();
-console.log(myDate)
+console.log(`toStringMethod :  ${myDate.toString()}`)
+// toStringMethod :  Sat Aug 24 2024 14:44:48 GMT+0000 (Coordinated Universal Time)
+console.log(`todateString   :  ${myDate.toDateString()}`)
+// todateString   :  Sat Aug 24 2024
+console.log(`toLocaleString :  ${myDate.toLocaleString()}`)
+// toLocaleString :  8/24/2024, 2:44:48 PM
+
+let setDateName=  new Date(2024,7,24)
+console.log(setDateName.toDateString())
+
+let setDateTimeName=  new Date(2024,7,24,8,23,)
+console.log(setDateTimeName.toLocaleString())
+
+let setCustomDate=  new Date("2023-08-24")
+console.log(setCustomDate.toLocaleString())
+
+// let setCustomDate=  new Date("March,05,1994")
+// console.log(setCustomDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(setCustomDate.getTime())
+
+console.log(Date.now()/1000)
+console.log(Math.floor(Date.now()/1000))
+
+// console.log(myTimeStamp.getDay())
+
+let curentDate=new Date()
+console.log(curentDate.getDay());
+
+// myTimeStamp.toLocaleString('default',{
+//     weekday:"long"
+// })
+
+let currentDate= new Date()
+console.log(currentDate.toLocaleString("default",{
+    weekday:"long"
+}))
