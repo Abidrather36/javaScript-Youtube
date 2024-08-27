5d139d765399ea7bbe3562406f08a0f1a4a0b488
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-27 17:02:44 +0000

    obj more

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,12 +1,19 @@
 // const obj = new Object();
 // console.log(obj)
-const aisleUser={
-    name:"Abid",
-    userName:{
-        fullName:{
-            firstName:"AbidNaseer",
-            lastName:"Rather"
-        }
-    }
-}
-console.log(aisleUser.userName.fullName)
+// const aisleUser={
+//     name:"Abid",
+//     userName:{
+//         fullName:{
+//             firstName:"AbidNaseer",
+//             lastName:"Rather"
+//         }
+//     }
+// }
+// console.log(aisleUser.userName.fullName)
+
+const obj1 ={1:"a",2:"b",3:"c"}
+const obj2 ={4:"d",5:"e",6:"f"}
+
+// const obj3={obj1 ,obj2}
+const obj3 =Object.assign({},obj1,obj2)
+console.log(obj3)
