e954a46fe4578eef77d3ec4101a7ca4c9ec3664f
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-27 16:49:52 +0000

    02 Object added

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+// const obj = new Object();
+// console.log(obj)
+const aisleUser={
+    name:"Abid",
+    userName:{
+        fullName:{
+            firstName:"AbidNaseer",
+            lastName:"Rather"
+        }
+    }
+}
+console.log(aisleUser.userName.fullName)
