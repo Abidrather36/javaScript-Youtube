d12fe7c00a462cf490b03e213f5a8a2237ba9172
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-29 17:01:53 +0000

    JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,26 +21,72 @@ console.log(obj3)
 
 /*later on data can be from database like an array of objects as below,so below is discussed how
 to access  it one is users.map and other is users[0] users[1] */
-const users=[{
-    id:"123@guid",
-    email:"a@gmail.com"
-},
-{
-    id:"1234@guid",
-    email:"b@google.com"
-},
-{
-    id:"12345@guid",
-    email:"c@yahoo.com"
-},
-{
-    id:"123456@guid",
-    email:"d@hotmail.com"
-}]
-console.log(users[3].email)
-console.log(Object.keys(users))
-console.log(Object.values(users))
-console.log(Object.values(aisleUser))
+// const users=[{
+//     id:"123@guid",
+//     email:"a@gmail.com"
+// },
+// {
+//     id:"1234@guid",
+//     email:"b@google.com"
+// },
+// {
+//     id:"12345@guid",
+//     email:"c@yahoo.com"
+// },
+// {
+//     id:"123456@guid",
+//     email:"d@hotmail.com"
+// }]
+// console.log(users[3].email)
+// console.log(Object.keys(users))
+// console.log(Object.values(users))
+// console.log(Object.values(aisleUser))
 
 // console.log(facebookProfile.hasOwnProperty('userName'))
 //  true
+
+
+//Destructuring of Object //
+
+const course={
+    courseName:"JavaScript",
+    courseFee:"1999",
+    courseId:103783528
+}
+const {courseFee :fee}=course
+console.log(fee)
+
+const user={
+    userName:"Abidrather36",
+    age:28,
+    location:"hrbr",
+    residence:"Athwajan Srinagar",
+    pinCode:560043
+}
+const {userName}=user
+console.log(userName) 
+const {pinCode }=user
+console.log(pinCode)
+const {residence:living} =user 
+console.log(living)
+//property of Ojects hasOwnProperty returns true or false for the parameter given 
+console.log(course.hasOwnProperty("courseFee"))
+// true
+
+/*JSON key values is in string except value for number and it depends sometimes JSON 
+can be in an araay  like 2nd example
+*/
+// Ist example
+{
+
+    "name":"Abid",
+     "Age":28,
+     "parentage":"Naseer"
+}
+
+// 2nd example
+[
+  {},
+  {},
+  {}
+]
