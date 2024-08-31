637a447845193417fe58984b373638e554dcef8c
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-08-31 15:10:34 +0000

    update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,16 +28,62 @@
 
 //Understanding undefined//
 
-function userlogging(username="default"){
-    if(username===undefined){
-        console.log("please enter your username");
-        return ;
-    }
-    else{
-        return `${username} you just logged in`
-    }
-}
+// function userlogging(username="default"){
+//     if(username===undefined){
+//         console.log("please enter your username");
+//         return ;
+//     }
+//     else{
+//         return `${username} you just logged in`
+//     }
+// }
 // userlogging()
 // "please enter your username"
-console.log(userlogging())
+// console.log(userlogging())
 // 'Abidrather36 you just logged in
+
+
+// function one(){
+//     const username="abidnaseer"
+
+//     function two(){
+//     const email ="abidnaseer@google.in"
+//     console.log(username)
+//     }
+//     two()
+//     // console.log(email)
+// }
+// one()
+if(true){
+const userName="abidnaseer36"
+if(userName==="abidnaseer36"){
+    const website =" chaiaurcode"
+    console.log(userName + website)
+}
+// console.log(website)
+
+}
+
+// console.log(userName)
+
+const result =function(value){   //function expression//
+    return value+1
+}
+undefined
+result(4)
+5
+result(122)
+123
+
+welcome()
+function welcome(){
+    console.log("welcome to js")
+}
+
+left()
+const left=function leaves(){
+    console.log("function left")
+}
+/* So at top when we declared function as with name welcome() and invoked before creation it hoisted 
+and return value means it ran, while with second function expression left() it throws error means 
+you cannot hoisted or invoke before creation of function expression */
