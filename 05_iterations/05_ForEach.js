5bd705e4378b1aad91f9890a741cd055f5b04000
Abid Naseer <100025828+Abidrather36@users.noreply.github.com>
2024-09-11 16:59:34 +0000

    reduce and map

--- 05_iterations/05_ForEach.js
+++ 05_iterations/05_ForEach.js
@@ -34,7 +34,7 @@
 // ]
 
 // myCoding.forEach( (item) => {
-    
+
 //     console.log(item.languageName);
 // } )
 
@@ -48,11 +48,11 @@
 // undefined foreach  returns undefined 
 
 // since we can't return from foreach ,so there is a jugaad for this create an array push them into that array/
-const myNums=[1,2,3,4,5,6,7,8,9,10]
-const newArray=[]
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newArray = []
 
-myNums.forEach((num)=>{
-    if(num >4){
+myNums.forEach((num) => {
+    if (num > 4) {
         newArray.push(num)
     }
 })
@@ -71,10 +71,10 @@ const books = [
     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
+];
 
-  let scienceBooks=books.filter((bk)=> bk.genre==="Science")
-  console.log(scienceBooks)
+let scienceBooks = books.filter((bk) => bk.genre === "Science")
+console.log(scienceBooks)
 
 
 
@@ -92,3 +92,60 @@ const numbers = [1, 2, 3, 4];
 // })
 
 // console.log(retHeroes)
+
+
+/*
+  Recap Foreach() takes callback and returns nothing undefined while filter()also takes 
+  callback but returns an array instead on a specific condition after that is map() that 
+  also takes callback function and returns an array and we can do chaining like map().map().filter
+
+*/
+
+const nums = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+
+const resArray = nums.map((num) => num * 10).map((num) => num + 1).filter((num) => num > 40)
+
+console.log(resArray)
+
+
+
+//Reducer()
+
+const arr = [2, 4, 6]
+
+const newArr = arr.reduce((acc, currVal) => {
+    console.log(`acc ${acc} currVal ${currVal}`)
+    return acc + currVal
+}, 2)
+
+console.log(newArr)
+
+
+//mini Project on reduce//
+
+const shoppingCart = [
+
+    {
+        itemName: "Python Course",
+        price: 2000
+    },
+    {
+        itemName: "Java Course",
+        price: 3000
+    },
+    {
+        itemName: "Dot Net Course",
+        price: 4000
+    },
+    {
+        itemName: "Php Course",
+        price: 1000
+    }
+
+]
+
+const totalPriceOfShoppingCart = shoppingCart.reduce((acc, item) => {
+     return acc+item.price
+}, 0)
+
+console.log(`Total price Payable of Courses is ${totalPriceOfShoppingCart}`)
